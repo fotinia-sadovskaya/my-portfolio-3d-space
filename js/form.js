38a947e753cea58003d50b925a77eb661837f434
Svitlana <106338548+fotinia-sadovskaya@users.noreply.github.com>
2023-08-09 20:10:29 +0300

    Update form.js

--- js/form.js
+++ js/form.js
@@ -27,7 +27,7 @@ function showShipAnim() {
 
   setTimeout(() => {
     shipEl.removeAttribute("src");
-  }, 4000);
+  }, 6000);
 }
 
 addShipElement();
@@ -52,7 +52,7 @@ form.addEventListener("submit", (e) => {
 
       setTimeout(() => {
         launchBtn.style.opacity = "1";
-      }, 4000);
+      }, 6000);
     })
     .catch((error) => console.log("Sending form failed"));
 });
